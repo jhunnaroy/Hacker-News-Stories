c10287e68f596c4d17af4f914d474962d6d1d936
Jhunna Kumar <jhunnaroy@gmail.com>
2026-05-08 23:45:08 +0530

    efdgh

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,11 +2,16 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (
+  req,
+  res
+) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password } =
+      req.body;
 
-    const userExists = await User.findOne({ email });
+    const userExists =
+      await User.findOne({ email });
 
     if (userExists) {
       return res.status(400).json({
@@ -14,7 +19,8 @@ export const registerUser = async (req, res) => {
       });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword =
+      await bcrypt.hash(password, 10);
 
     const user = await User.create({
       name,
@@ -27,15 +33,25 @@ export const registerUser = async (req, res) => {
       user,
     });
   } catch (error) {
-    res.status(500).json(error);
+    console.log(error);
+
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
 
-export const loginUser = async (req, res) => {
+export const loginUser = async (
+  req,
+  res
+) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } =
+      req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({
+      email,
+    });
 
     if (!user) {
       return res.status(400).json({
@@ -43,10 +59,11 @@ export const loginUser = async (req, res) => {
       });
     }
 
-    const isMatch = await bcrypt.compare(
-      password,
-      user.password
-    );
+    const isMatch =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
 
     if (!isMatch) {
       return res.status(400).json({
@@ -69,18 +86,30 @@ export const loginUser = async (req, res) => {
       user,
     });
   } catch (error) {
-    res.status(500).json(error);
+    console.log(error);
+
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
 
-export const logoutUser = async (req, res) => {
+export const logoutUser = async (
+  req,
+  res
+) => {
   try {
     res.json({
       token: null,
       user: null,
-      message: "Logged out successfully"
-       });
+      message:
+        "Logged out successfully",
+    });
   } catch (error) {
-    res.status(500).json(error);
+    console.log(error);
+
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
